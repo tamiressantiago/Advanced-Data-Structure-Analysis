6d559d07c83a129319c596bdb8a8bac1ea5bba10
Maria Eduarda Viana <83471555+eduardaviana@users.noreply.github.com>
2025-03-12 14:49:42 -0300

    Adição da classe Btree

--- src/main/java/advanced-data-structure/structure/Btree.java
+++ src/main/java/advanced-data-structure/structure/Btree.java
@@ -1,1 +1,45 @@
 package structure;
+public class BTree {
+    private BTreeNode root;
+    private int t;
+
+    public BTree(int t) {
+        this.t = t;
+        root = null;
+    }
+
+    public BTreeNode search(int key) {
+        return (root == null) ? null : root.search(key);
+    }
+
+    public void insert(int key) {
+        if (root == null) {
+            root = new BTreeNode(t, true);
+            root.keys[0] = key;
+            root.n = 1;
+        } else {
+            if (root.n == 2 * t - 1) {
+                BTreeNode newRoot = new BTreeNode(t, false);
+                newRoot.children[0] = root;
+                newRoot.splitChild(0, root);
+                int i = (newRoot.keys[0] < key) ? 1 : 0;
+                newRoot.children[i].insertNonFull(key);
+                root = newRoot;
+            } else {
+                root.insertNonFull(key);
+            }
+        }
+    }
+
+    public void remove(int key) {
+        if (root != null)
+            root.remove(key);
+    }
+
+    public void printBTree() {
+        if (root != null)
+            root.printInOrder();
+        System.out.println();
+    }
+    
+}
